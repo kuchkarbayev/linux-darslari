889b3bedfb5c982e8718b38c7b605afe9ee52fde
Ahrorbek Qochqarboyev <87897224+kuchkarbayev@users.noreply.github.com>
2025-12-10 22:22:10 +0500

    test fayl
    test fayl

---
+++ test.js
@@ -0,0 +1,30 @@
+const {remote} = require('webdriverio');
+
+const capabilities = {
+  platformName: 'Android',
+  'appium:automationName': 'UiAutomator2',
+  'appium:deviceName': 'Android',
+  'appium:appPackage': 'com.android.settings',
+  'appium:appActivity': '.Settings',
+};
+
+const wdOpts = {
+  hostname: process.env.APPIUM_HOST || 'localhost',
+  port: parseInt(process.env.APPIUM_PORT, 10) || 4723,
+  path: '/wd/hub',
+  logLevel: 'info',
+  capabilities,
+};
+
+async function runTest() {
+  const driver = await remote(wdOpts);
+  try {
+    const appsItem = await driver.$('//*[@text="Apps"]');
+    await appsItem.click();
+  } finally {
+    await driver.pause(1000);
+    await driver.deleteSession();
+  }
+}
+
+runTest().catch(console.error);
